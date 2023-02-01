5ed6b3de286f1f17e827bee1a7f243df01f6dd12
Mateus Paiva <mateusopaiva@gmail.com>
2023-02-01 14:21:16 -0300

    Aula 03 - 6 Edição

--- aluraflix/index.js
+++ aluraflix/index.js
@@ -1,9 +1,23 @@
-var listaFilmes = [
-    "https://upload.wikimedia.org/wikipedia/pt/7/79/Yesterday_%282019%29_poster.jpg",
-    "https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
-    "https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/91/90/98/20169244.jpg"
-];
-
-for(var i = 0; i < listaFilmes.length; i++) {
-    document.write("<img src=" + listaFilmes[i] + ">")
+function adicionarFilme() {
+    var filmeFavorito = document.getElementById("filme").value;
+    if(filmeFavorito.endsWith(".jpg")) {
+        listarFilmesNaTela(filmeFavorito);
+    } else {
+        console.error("Endereço de filme invalido");
+    }
+    document.getElementById("filme").value = "";
 }
+
+function listarFilmesNaTela(filmeFavorito) {
+    var elementoFilmeFavorito = "<img src=" + filmeFavorito + ">";
+    var elementolistaFilmes = document.getElementById("listaFilmes");
+    elementolistaFilmes.innerHTML = elementolistaFilmes.innerHTML + elementoFilmeFavorito;
+}
+
+/* EXEMPLOS DE ENDEREÇOS DE FILMES:
+    https://br.web.img2.acsta.net/pictures/bzp/01/61282.jpg
+    https://br.web.img3.acsta.net/pictures/19/04/26/17/30/2428965.jpg
+    https://br.web.img2.acsta.net/pictures/22/08/23/09/37/1541299.jpg
+    https://br.web.img3.acsta.net/pictures/15/10/18/18/39/408904.jpg
+    https://br.web.img2.acsta.net/r_1280_720/img/0e/85/0e85b4029c87236712cf11cd3a343ae2.jpg
+*/
